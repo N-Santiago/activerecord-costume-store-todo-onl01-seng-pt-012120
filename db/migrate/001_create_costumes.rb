37ae28f0a2af01d7c618707987c599df267c4ffb
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-31 20:09:35 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,13 +1,5 @@
 class CreateCustomes < ActiveRecord::Migration[5.1]
   
-  def change
-   create_table :customes do |t|
-      t.string :name 
-      t.integer :price
-      t.string :image_url
-      t.string :size
-      t.timestamps
-    end
-  end
+
   
 end
